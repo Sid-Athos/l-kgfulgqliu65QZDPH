d4925dde680ad52605400b3252a0b5b23c87fa9b
sid-athos <sa.bennaceur@gmail.com>
2018-06-18 00:37:53 +0200

    navbar fix

--- maths/maths_views/navbar.php
+++ maths/maths_views/navbar.php
@@ -1,16 +1,7 @@
 <div class="topnav" id="myTopnav">
   <a href="../maths/occurences.php" class="active">Occurences</a>
-<<<<<<< HEAD
-  <a href="../maths/decypher.php">Cesar</a>
-  <a href="#contact">Machine Enigma</a>
-  <a href="#about">Boustrophedon</a>
-=======
   <a href="../maths/decypher.php">César</a>
   <a href="#contact">Machine Enigma</a>
   <a href="#about">Boustrophédon</a>
->>>>>>> 17da0aa15ce0914334ebaccd915c63846ad360f5
-  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-    <i class="fa fa-bars"></i>
-  </a>
 </div>
 <body>
