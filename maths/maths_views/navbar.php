17da0aa15ce0914334ebaccd915c63846ad360f5
sid-athos <sa.bennaceur@gmail.com>
2018-06-18 00:27:51 +0200

    stable_versions

--- maths/maths_views/navbar.php
+++ maths/maths_views/navbar.php
@@ -2,7 +2,7 @@
   <a href="../maths/occurences.php" class="active">Occurences</a>
   <a href="../maths/decypher.php">César</a>
   <a href="#contact">Machine Enigma</a>
-  <a href="#about">Boustrophedon</a>
+  <a href="#about">Boustrophédon</a>
   <a href="javascript:void(0);" class="icon" onclick="myFunction()">
     <i class="fa fa-bars"></i>
   </a>
