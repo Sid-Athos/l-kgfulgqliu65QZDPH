cd15a158530fff8b6aad9c8110a2949661750eda
sid-athos <sa.bennaceur@gmail.com>
2018-06-20 20:10:22 +0200

    lol

--- veterinaires/eval_fs/Controllers/login.php
+++ veterinaires/eval_fs/Controllers/login.php
@@ -1,3 +1,43 @@
 <?php
 
 include_once("./Models/db_connect.php");
+
+$error = false;
+$flag_email_taken = false;
+$flag_name_taken = false;
+// check if form is submitted
+
+switch(isset($_POST['register'])):  
+    case 'Register':
+    $email = htmlspecialchars(trim($_POST['email']), ENT_QUOTES, 'UTF-8');
+    $password = $_POST['password'];
+
+    if(!filter_var($email,FILTER_VALIDATE_EMAIL)) {
+        $error = true;
+        $email_error = "Entrez une addresse email valide";
+    }
+    if(strlen($password) < 6) {
+        $error = true;
+        $password_error = "6 caractères ou plus";
+    }
+    
+    if (!$error) {
+        // check if the combination fname/lname/email is already used
+        include('./Models/log_check.php');
+        $row = $stmt->fetch();
+        if($row){
+            $_SESSION['user_ID'] = $row['ID'];
+        } else {
+                    // Add row to database
+                    include('./Controllers/Functions/PHP/backup_clients.php');
+                    include('./Models/register_clients.php');
+                    $successmsg = "Vous êtes inscrit! <a href='index.php?page=login' class='alert-link'></br>Cliquez ici pour vous connecter</a>";
+                }
+        }
+        break;
+
+        default:
+    endswitch;
+    include('./Views/templates/login_form.php');
+
+?>
---
+++ veterinaires/eval_fs/Views/templates/login_form.php
@@ -0,0 +1,34 @@
+<div class="row">
+        <div class="col-md-4 col-md-offset-4 text-center">
+        <br>Pas encore inscrit ? <a href="./index.php?page=Inscription">Vous pouvez le faire ici</a>
+        </div>
+    </div>
+<div class="container"  style="margin-top: 20px;">
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4" style="max-height= 600px overflow-x= scroll">
+            <form role="form" class="form container-fluid border border-dark rounded" action="index.php?page=Inscription" method="POST" name="register_form">
+                    <?php if ($flag_email_taken) { alert($email_taken); } ?>
+                    <?php if ($flag_name_taken) { alert($name_taken); } ?>
+                    <?php if (isset($successmsg)) { success($successmsg); } ?>
+                    <?php if (isset($errormsg)) { alert($errormsg); } ?>
+                <fieldset class="well">
+                    <h4 style="padding: 8px;">Inscription</h4>
+                    <div class="form-group">
+                        <label for="name">Email</label>
+                        <input type="email" class="form-control space-bottom" name="email" placeholder="Email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" required value="<?php if($error || $flag_email_taken || $flag_name_taken) echo $email; ?>" />
+                        <?php if (isset($email_error)) alert($email_error); ?>
+                    </div>
+                    <div class="form-group">
+                        <label for="name">Mot de passe</label>
+                        <input type="password" class="form-control space-bottom" name="password" placeholder="Mot de passe" pattern=".{6,}" title="Au moins 6 charactères" required/>
+                        <?php if (isset($password_error)) alert($password_error); ?>
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" class="btn btn-block btn-primary space-bottom" name="register" value="Register" />
+                    </div>
+                </fieldset>
+            </form>
+        </div>
+    </div>
+    
+</div>
