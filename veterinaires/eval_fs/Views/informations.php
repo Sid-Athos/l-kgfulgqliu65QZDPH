e5d7d796488c4551628c347ca29dc5e3ed33fb9d
sid-athos <sa.bennaceur@gmail.com>
2018-06-20 13:37:19 +0200

    veteriaires

--- veterinaires/eval_fs/Views/informations.php
+++ veterinaires/eval_fs/Views/informations.php
@@ -1,3 +1,5 @@
+<br><h1> Bienvenue sur le site de Heal mon Bichon, la clinique vétérinaire de référence à Ivry-sur-Seine</h1><br>
+
 <h1> Le cabinet est ouvert tous les jours de la semaine, de 8h à 19h30. Nocturne le premier dimanche de chaque mois.<h1><br>
 <p>Nous sommes situés au 74, avenue Maurice Thorez à Ivry-sur-Seine</p<
 <p> Vous pouvez facilement accèder à notre clinique via le métro 7 (arrêt Pierre et Marie Curie).<br> <br>Des correspondances vers le tramway sont disponibles
