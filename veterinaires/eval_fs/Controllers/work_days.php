4bf36d69d4ced7b065f0d254b62aaaa435ef8d44
sid-athos <sa.bennaceur@gmail.com>
2018-06-22 01:43:55 +0200

    ouioui

--- veterinaires/eval_fs/Controllers/work_days.php
+++ veterinaires/eval_fs/Controllers/work_days.php
@@ -22,9 +22,38 @@
             include('./Views/templates/add_new_workday.php');
         break;
         case(isset($_POST['edit'])):
+            include('./Models/show_workdays.php');
+            $work_days = $stmt -> fetchAll();
+            for($i=0;$i<count($work_days);$i++){
+                $working_days[] = implode("",$work_days[$i]);
+            }
+            $working_days = implode("",$working_days);
+            $days = array("Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche");
+            for($i=0;$i<count($days);$i++){
+                if(!strchr($working_days,$days[$i])){
+                    $days_available[] = $days[$i];
+                }
+            }
+            include('./Views/templates/edit_workday.php');
         break;
         case(isset($_POST['delete'])):
+            include('./Models/select_workdays.php');
+            $work_days = $stmt -> fetchAll();
+            include('./Views/templates/suppress_workday_form.php');
         break;
+        case(isset($_POST['add_day'])):
+            $days_free = $_POST['days_add'];
+            $from_time = $_POST['from_hour'].':'.$_POST['from_min'].':00';
+            $to_time = $_POST['to_hour'].':'.$_POST['to_min'].':00';
+            if((int)$_POST['from_hour'] >= (int)$_POST['to_hour']){
+                $error_add = "L'heure de début doit être supérieure à celle de fin";
+            }
+            include('./Models/add_working_day.php');
+        break;
+        case(isset($_POST['optradio'])):
+            include('./Models/delete_day.php');
+            $successmsg ="Suppression confirmée";
+            break;
         default:
     endswitch;
 
@@ -36,18 +65,3 @@
     }
 
 ?>
-
-$i=0;
-while($i<count($tab[$i])){
-    $working_days[] = implode("",$table_rows[$i]);
-    $i++;
-}
-$working_days = implode("",$working_days);
-echo($tabi);
-
-$days = array("Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche");
-for($i=0;$i<count($days);$i++){
-    if(strchr($working_days,$days[$i])== false){
-        $days_available[] = $days[$i];
-    }
-}
